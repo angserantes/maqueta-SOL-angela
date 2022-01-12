590c7f9338cecf56a898455ea8c538458b51fa59
angeserantes <aserantes7@gmail.com>
2022-01-12 13:03:28 +0100

    cambios

---
+++ js/header.js
@@ -0,0 +1,59 @@
+const headerTemplate = document.createElement('template');
+
+headerTemplate.innerHTML = `
+  <style>
+    nav {
+      height: 40px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      background-color:  #B81207;
+    }
+
+    ul {
+      padding: 0;
+    }
+    
+    ul li {
+      list-style: none;
+      display: inline;
+    }
+    
+    a {
+      font-weight: 700;
+      margin: 0 25px;
+      color: #fff;
+      text-decoration: none;
+    }
+    
+    a:hover {
+      padding-bottom: 5px;
+      box-shadow: inset 0 -2px 0 0 #fff;
+    }
+  </style>
+  <header>
+    <nav>
+      <ul>
+        <li><a href="index.html">Inicio</a></li>
+        <li><a href="citas.html">Citas</a></li>
+        <li><a href="empresas.html">Empresas</a></li>
+        <li><a href="personas.html">Personas</a></li>
+        <li><a href="usuario.html">Usuario</a></li>
+      </ul>
+    </nav>
+  </header>
+`;
+
+class Header extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    const shadowRoot = this.attachShadow({ mode: 'closed' });
+
+    shadowRoot.appendChild(headerTemplate.content);
+  }
+}
+
+customElements.define('header-component', Header);
